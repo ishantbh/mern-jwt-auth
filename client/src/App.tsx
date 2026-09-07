702317f24e84108c3b98dfc0c71aa5aff109f9bb
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-09-07 21:58:34 +0530

    setup theme toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from './components/ui/button'
+import { ThemeToggle } from './components/theme-toggle'
 
 export default function App() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Chat App</h1>
-      <Button>Click me</Button>
+      <ThemeToggle />
     </div>
   )
 }
