efa64fa3eae45c3d7c16d6e76c588230ec09fd17
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-09-07 22:09:16 +0530

    setup routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { ThemeToggle } from './components/theme-toggle'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Login from './pages/login'
+import Register from './pages/register'
+import Dashboard from './pages/dashboard'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Chat App</h1>
-      <ThemeToggle />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Chat App</div>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
