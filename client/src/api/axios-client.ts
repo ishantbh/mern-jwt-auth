feecd7485853a80adab156e53e5e7b4acb74bfc8
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-09-09 19:33:50 +0530

    add response interceptor to handle token refresh on 401 errors

--- client/src/api/axios-client.ts
+++ client/src/api/axios-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { type AxiosError, type InternalAxiosRequestConfig } from 'axios'
 import { useBoundStore } from '../store'
 
 const axiosClient = axios.create({
@@ -13,5 +13,50 @@ axiosClient.interceptors.request.use((config) => {
   }
   return config
 })
+
+// Track retry status
+interface RetriableConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean
+}
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as RetriableConfig | undefined
+
+    if (!originalRequest) {
+      return Promise.reject(error)
+    }
+
+    const isAuthEndpoint = originalRequest?.url?.includes('/auth')
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !isAuthEndpoint
+    ) {
+      originalRequest._retry = true
+
+      try {
+        const { data } = await axios.post<{ accessToken: string }>(
+          '/api/auth/refresh',
+          {},
+          { withCredentials: true },
+        )
+
+        useBoundStore.getState().setAccessToken(data.accessToken)
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`
+
+        return axiosClient(originalRequest)
+      } catch (refreshError) {
+        useBoundStore.getState().clearAuth()
+        window.location.href = '/login'
+        return Promise.reject(refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
 
 export default axiosClient
