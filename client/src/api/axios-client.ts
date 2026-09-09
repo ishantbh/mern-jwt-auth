cc3ef4c6ad0f2eb5114d3069ef122c82c0f7f7b1
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-09-09 18:51:04 +0530

    add request interceptor to axios client for authorization token

--- client/src/api/axios-client.ts
+++ client/src/api/axios-client.ts
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import { useBoundStore } from '../store'
 
 const axiosClient = axios.create({
   baseURL: '/api',
   withCredentials: true,
+})
+
+axiosClient.interceptors.request.use((config) => {
+  const token = useBoundStore.getState().accessToken
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 export default axiosClient
